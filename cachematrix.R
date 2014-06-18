b14510e85fb8f0f5c482f5281c736e2fed76319f
Ken Jon Yeong <kyeong16@jcu.edu>
2014-06-18 12:38:25 -0400

    commiting homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix allows you to "store" a matrix so that you can recall it later
+## Let x be a matrix. then the code mat<-makeCacheMatrix(x) stores
+## the matrix x. 
+## Let y be another matrix. 
+##  The function mat$set(y) allows you to replace the matrix x with another matrix y. 
+##  mat$get() returns the matrix that is "stored"
+##  mat$setInverse(inverse) allows you to input the inverse of the stored matrix
+##  mat$getInverse() returns the inverse that is stored
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x <<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setInverse<-function(inverse) m<<-inverse
+        getInverse<-function()m
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)       
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the "stored" matrix x in mat using mat$setInverse.
+## Otherwise, it computes and returns the inverse of x using the solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getInverse()
+        if(!is.null(m)){
+                message("getting Inverse of Matrix")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setInverse(m)
+        m
 }
